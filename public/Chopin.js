290e21f2db6bed7f55c47a0e529cbc9f084ddc36
ariarun <64533642+ariarun@users.noreply.github.com>
2024-04-30 15:23:22 -0700

    4 songs added
    More songs added, but the AI js file need to change, it all use cannon chat

---
+++ public/Chopin.js
@@ -0,0 +1,17 @@
+import './assets/pspdfkit.js';
+
+// We need to inform PSPDFKit where to look for its library assets, i.e. the location of the `pspdfkit-lib` directory.
+const baseUrl = `${window.location.protocol}//${window.location.host}/assets/`;
+
+PSPDFKit.load({
+	baseUrl,
+	licenseKey: 'KBfkvGnjghp1eLkWNhmj_RGTy-3iDPs0xkc4ZAIwQJcP2fYcnwpV4ZEmtXPqJkAy9RbQOh4Rb89PR3ZBYiqNDyPNFB6Ey_tnSVxl9tCDGkGwFoIfUYdYYRZ6fxuASQajb7oVefIUwnxFdY51cDRAo2eXH2bvmGRyGbq66UWleprFI3IsPxFVkgj50RgUDHbzfwdwKZW5qh7kfM8a',
+	container: '#pspdfkit',
+	document: './music_source/Chopin-Funeral_March/Chopin_-_Funeral_March.pdf',
+})
+	.then((instance) => {
+		console.log('PSPDFKit loaded', instance);
+	})
+	.catch((error) => {
+		console.error(error.message);
+	});
--- public/gallery.js
+++ public/gallery.js
@@ -7,7 +7,7 @@ PSPDFKit.load({
 	baseUrl,
 	licenseKey: 'KBfkvGnjghp1eLkWNhmj_RGTy-3iDPs0xkc4ZAIwQJcP2fYcnwpV4ZEmtXPqJkAy9RbQOh4Rb89PR3ZBYiqNDyPNFB6Ey_tnSVxl9tCDGkGwFoIfUYdYYRZ6fxuASQajb7oVefIUwnxFdY51cDRAo2eXH2bvmGRyGbq66UWleprFI3IsPxFVkgj50RgUDHbzfwdwKZW5qh7kfM8a',
 	container: '#pspdfkit',
-	document: './music_source/cannon/Cannon_in_D.pdf',
+	document: "../music_source/cannon/Cannon_in_D.pdf",
 })
 	.then((instance) => {
 		console.log('PSPDFKit loaded', instance);
