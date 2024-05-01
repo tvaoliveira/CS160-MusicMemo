a5bbf506a5ce808c188a70b9b314af6619e9833d
Hunter <huntercc742@gmail.com>
2024-05-01 13:15:51 -0700

    Removed highlighting of login nav button for now.

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -6,29 +6,29 @@ function Nav() {
 
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <img src={imgs.logo_transparent} width="85" height="60" alt=""/>
+      <img src={imgs.logo_transparent} width="85" height="60" alt="" />
 
-        <a class="navbar-brand" href="/" style={{paddingLeft: '1rem'}}>Music Memo</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+      <a class="navbar-brand" href="/" style={{ paddingLeft: '1rem' }}>Music Memo</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
 
-              <Link class="nav-link" to="/">Home</Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/signUp">Sign Up</Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/login" style={{color: 'rgb(235, 140, 152)'}}>Login<span
-                class="sr-only"></span></Link>
-            </li>
-          </ul>
-        </div>
+            <Link class="nav-link" to="/">Home</Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/signUp">Sign Up</Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/login">Login<span
+              class="sr-only"></span></Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 
